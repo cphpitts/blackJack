801cdd4c5dcac9963d2922faecf87da6a3917a87
cphpitts <cphpitts@users.noreply.github.com>
2020-06-15 15:31:05 -0700

    Add ACME Accounting Exercise

---
+++ Basic C# Projects/AcmeAccounting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter User Name: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Welcome, " + login + " to Acme");
+            Console.WriteLine("Remember, we're \"accounting\" on you");
+            Console.Read();
+        }
+    }
+}
