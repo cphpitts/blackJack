255cb7146481f4cb18e87168bb000ddc327dd9c6
cphpitts <cphpitts@users.noreply.github.com>
2020-06-16 16:07:23 -0700

    Add While Exercise

---
+++ Basic C# Projects/WhileExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhileExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Set initial value
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            } while (i < 10);
+
+            Console.WriteLine("\n");
+
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i = i - 2;
+            }
+
+            Console.Read();
+        }
+    }
+}
