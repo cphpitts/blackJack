1eec41905b185c716b26a1c3b236353c3ec00ba7
cphpitts <cphpitts@users.noreply.github.com>
2020-06-18 16:48:28 -0700

    Add class exercise

---
+++ Basic C# Projects/ClassExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int doubleResult = numMachine.DoubleNum(number);
+            double halveResult = numMachine.Halve(number);
+            string evenOddResult = numMachine.EvenOdd(number);
+
+            Console.WriteLine(number + " times 2 = " + doubleResult);
+            Console.WriteLine(number + " divided by 2 = " + halveResult);
+            Console.WriteLine(number + " is an " + evenOddResult + " number");
+            Console.ReadLine();
+
+
+        }
+    }
+}
