49e67951fa6ff741e62d87359294717bb20b42fd
cphpitts <cphpitts@users.noreply.github.com>
2020-06-19 08:39:02 -0700

    Add Method Overload Exercise

---
+++ Basic C# Projects/OverloadExercise/mathBot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadExercise
+{
+    class mathBot
+    {
+        public static int calcutron(int number)
+        {
+            int result = number * 10;
+            return result;
+        }
+
+        public static int calcutron(decimal number)
+        {
+            int result = Convert.ToInt32(number) + 5;
+            return result;
+        }
+
+        public static int calcutron(string number)
+        {
+            int result = Convert.ToInt32(number) + 50;
+            return result;
+        }
+        
+
+    }
+}
