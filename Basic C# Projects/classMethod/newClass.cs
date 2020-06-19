3c86dacdfc649ccc2c7d81772938b3409877fc40
cphpitts <cphpitts@users.noreply.github.com>
2020-06-19 11:24:11 -0700

    Add Class Method exercise

---
+++ Basic C# Projects/classMethod/newClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod
+{
+    class newClass
+    {
+        public int value { get; set; }
+
+        public void Divvy(out int result)
+        {
+            result = this.value / 2;
+        }
+
+        public void Divvy(int divisor, out int result)
+        {
+            result = value / divisor;
+        }
+
+        public static int Multy(int number)
+        {
+            int result = number * 3;
+            return result;
+        }
+    }
+}
