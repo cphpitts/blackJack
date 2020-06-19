76997d5312af5ad354a116e844c36f81264ca6c8
cphpitts <cphpitts@users.noreply.github.com>
2020-06-19 10:31:44 -0700

    Add void/ out parameter exercise

---
+++ Basic C# Projects/methodExercise/Thing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodExercise
+{
+    class Thing
+    {
+        public static void Thingify(int number, out int result, int displayNum)
+        {
+            result = number * displayNum;
+            Console.WriteLine("Second integer: {0}", displayNum);
+        }
+        
+    }
+}
