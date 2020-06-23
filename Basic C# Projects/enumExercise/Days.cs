c3aaecc9927d025571f8fb77640f36a976c4767b
cphpitts <cphpitts@users.noreply.github.com>
2020-06-23 10:33:00 -0700

    Add enum exercise. For real this time

---
+++ Basic C# Projects/enumExercise/Days.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExercise
+{
+    public class Days
+    {
+        public Name Name { set; get; }
+    }
+    public enum Name
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Basic C# Projects/enumExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it?");
+            string today = Console.ReadLine();
+            try
+            {
+                if (Enum.IsDefined(typeof(Name), today))
+                {
+                    Console.WriteLine("That's great. Have a nice day.");
+                }
+                else
+                {
+                    throw new ApplicationException("Given day not in enum");
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
