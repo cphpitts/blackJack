05eaa74d802d971701add830d184306a153538b4
cphpitts <cphpitts@users.noreply.github.com>
2020-06-25 11:22:35 -0700

    Add logging exercise

---
+++ Basic C# Projects/Logging/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class Logger
+    {
+        public static void LogWriter(int number)
+        {
+            using (StreamWriter file = new StreamWriter(@"C:\Users\cphpi\Documents\GitHub\cSharp_Projects\Basic C# Projects\Logging\Log\log.txt", true))
+            {
+                file.WriteLine(number);
+            }
+        }
+
+        public static string LogReader()
+        {
+            string logFile = File.ReadAllText(@"C:\Users\cphpi\Documents\GitHub\cSharp_Projects\Basic C# Projects\Logging\Log\log.txt");
+            return logFile;
+        }
+    }
+}
