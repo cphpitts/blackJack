ec3cd9ccbc9e19032e891041bdf44d096fe6056a
cphpitts <cphpitts@users.noreply.github.com>
2020-06-30 10:49:32 -0700

    Add exercise

---
+++ Basic C# Projects/Exercise/Operation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Operation
+    {
+        public Operation(string name, string target) : this(name, target, 4) 
+        { 
+        }
+        public Operation(string name, string target, int size)
+        {
+            opName = name;
+            opTarget = target;
+            opSize = size;
+        }
+        public string opName { get; set; }
+        public string opTarget { get; set; }
+        public int opSize { get; set; }
+
+        public string DescribeOp()
+        {
+            string opDesc = String.Format("Operation: {0} will require a {2}-person team to procure some {1}", this.opName, this.opTarget, this.opSize);
+            return opDesc;
+        }
+
+    }
+}
---
+++ Basic C# Projects/Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string operationName = "Roaring Dragon Thunder";
+            //operationName = "Fire Falcon"; //Can't redefine constant
+
+            var target = "Chcoclate Ice Cream";
+
+            Operation operation = new Operation(operationName, target);
+            string description = operation.DescribeOp();
+            Console.WriteLine(description);
+            Console.ReadLine();
+        }
+    }
+}
