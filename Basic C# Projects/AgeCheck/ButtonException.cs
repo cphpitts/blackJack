ecb236151971b8916d9556b1f8f872474ef507a4
cphpitts <cphpitts@users.noreply.github.com>
2020-06-30 14:07:28 -0700

    Add age exercise

---
+++ Basic C# Projects/AgeCheck/ButtonException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeCheck
+{
+    public class ButtonException : Exception
+    {
+        public ButtonException() : base() { }
+        public ButtonException(string message) : base(message) { }
+    }
+}
